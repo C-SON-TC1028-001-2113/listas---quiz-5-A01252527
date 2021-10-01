bd9715d05ab77dd109c826a58124eae0832251fd
A01252527 <a01252527@itesm.mx>
2021-10-01 17:10:16 +0000

    subida

--- assignments/16DiagonalPrincipal/src/exercise.py
+++ assignments/16DiagonalPrincipal/src/exercise.py
@@ -1,6 +1,29 @@
 
+
+def diagonal(matriz):
+    diagonal = []
+    for i in range(len(matriz)):
+        diagonal.append(matriz[i][i])
+    return diagonal
+
+def mati (Y,X):
+    matriz = []
+    if Y == X:
+        for i in range(Y):
+            matriz.append([])
+            for j in range (Y):
+                matriz[i].append(int(input()))
+    return matriz
+
 def main():
-  
+    X = int(input())
+    Y = int(input())
+    if X != Y:
+        print("La matriz no es cuadrada")
+    else:
+        MATRIZ = mati(X, Y)
+        print(diagonal(MATRIZ))
+
 
 if __name__=='__main__':
     main()
